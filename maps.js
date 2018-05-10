43b62bcae4f5f4e132c9983f49679ccdfcdd9df3
John <sir.sivoglaz2012@gmail.com>
2018-05-11 00:25:42 +0300

    Change city and street, maps settings

--- maps.js
+++ maps.js
@@ -1,11 +1,35 @@
 function initMap() {
-	var uluru = {lat: 50.435932, lng: 30.5205533};
+	var work = {lat: 50.435932, lng: 30.5205533};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 16,
-		center: uluru
+		zoom: 8,
+		center: work
 	});
 	var marker = new google.maps.Marker({
-		position: uluru,
+		position: work,
 		map: map
 	});
 }
+
+var contentString = '<div id="content">Тут всё то про что должно быть рассказано</div>';
+
+var infowindow = new google.maps.InfoWindow({
+	position: work,
+	map: map,
+	title: 'Work (Kiev)'
+});
+
+google.maps.event.addListener(marker, 'click', function(){
+	infowindow.open(map,marker);
+});
+
+var markers = []; 
+var marker = new google.maps.Marker({    
+	position: work
+});
+markers.push(marker);
+markerClusterer = new MarkerClusterer(_this.map, markers, 
+{ 
+	maxZoom: 13,
+	gridSize: 50,
+	styles: null 
+});
