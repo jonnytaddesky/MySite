8cf2ddbbf522b2e2915c894a1e116a17ab9ab35d
John <sir.sivoglaz2012@gmail.com>
2018-05-16 00:07:22 +0300

    add street view

--- maps.js
+++ maps.js
@@ -1,35 +1,20 @@
 function initMap() {
 	var work = {lat: 50.435932, lng: 30.5205533};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 8,
+		zoom: 16,
 		center: work
 	});
 	var marker = new google.maps.Marker({
 		position: work,
 		map: map
 	});
-}
-
-var contentString = '<div id="content">Тут всё то про что должно быть рассказано</div>';
-
-var infowindow = new google.maps.InfoWindow({
-	position: work,
-	map: map,
-	title: 'Work (Kiev)'
-});
-
-google.maps.event.addListener(marker, 'click', function(){
-	infowindow.open(map,marker);
-});
-
-var markers = []; 
-var marker = new google.maps.Marker({    
-	position: work
-});
-markers.push(marker);
-markerClusterer = new MarkerClusterer(_this.map, markers, 
-{ 
-	maxZoom: 13,
-	gridSize: 50,
-	styles: null 
-});
+	var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), {
+              position: work,
+              pov: {
+                heading: 34,
+                pitch: 10
+              }
+            });
+        map.setStreetView(panorama);
+} 
